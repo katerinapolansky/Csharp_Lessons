8dd2102c1d46d2c0e1073d16f5ad5817a59d0ef4
Oleksiy <oleksiy.kondratyuk.work@gmail.com>
2024-09-06 23:43:27 +0300

    function From1To30 was added

--- Csharp_lessons/Program.cs
+++ Csharp_lessons/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using InitialLesson;
 
-Console.WriteLine("Hello, World!");
+var initLesson = new Class1();
+initLesson.From1To30();
+
+Console.ReadKey();
---
+++ InitialLesson/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace InitialLesson;
+
+public class Class1
+{
+    public Class1()
+    {
+        
+    }
+    public void From1To30()
+    {
+        for (int i = 1; i < 31; i++) 
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
