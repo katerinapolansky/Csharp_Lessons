d36ae6d9da215488e4ff5dfa44c9061ddf108f54
Oleksiy <oleksiy.kondratyuk.work@gmail.com>
2024-09-14 13:26:39 +0300

    Add task Sept 14 lesson 2 user flow

--- Csharp_lessons/Program.cs
+++ Csharp_lessons/Program.cs
@@ -3,18 +3,9 @@ using HW1;
 using InitialLesson;
 using Lesson2;
 
-var initLesson = new Class1();
-initLesson.From1To30();
-var practice = new Practice();
-practice.ConvertString();
-practice.Convert2();
-var homework1 = new Homework1();
-homework1.BasicDataTypes();
-homework1.TypeConversion();
-homework1.Strings();
-homework1.StringInterpolation();
-homework1.StringFormatting();
-homework1.CustomStringFormatting();
+var lesson2 = new Practice();
+lesson2.UserFlow();
+
 
 
 Console.ReadKey();
