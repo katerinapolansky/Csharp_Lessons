32433c24730d62405c7fc7e042975940613414a7
katerinapolianska <katerinapolansky@gmail.com>
2024-09-14 10:36:49 +0100

    homework 1

--- Csharp_lessons/Program.cs
+++ Csharp_lessons/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using HW1;
 using InitialLesson;
 using Lesson2;
 
@@ -7,5 +8,13 @@ initLesson.From1To30();
 var practice = new Practice();
 practice.ConvertString();
 practice.Convert2();
+var homework1 = new Homework1();
+homework1.BasicDataTypes();
+homework1.TypeConversion();
+homework1.Strings();
+homework1.StringInterpolation();
+homework1.StringFormatting();
+homework1.CustomStringFormatting();
+
 
 Console.ReadKey();
