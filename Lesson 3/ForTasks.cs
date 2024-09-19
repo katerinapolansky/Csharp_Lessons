0039a082623cd2c0d7c70c86de244cf8559e22de
Yakiv Yasko <yasko_yakiv@charityhero.live>
2024-09-19 23:12:27 +0300

    for 1

---
+++ Lesson 3/ForTasks.cs
@@ -0,0 +1,18 @@
+﻿namespace Lesson_3;
+
+public class ForTasks
+{
+    public void For1Task()
+    {
+        Console.Write("Write n : ");
+        var n = int.Parse(Console.ReadLine());
+
+        Console.Write("Write n : ");
+        var k = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(k);
+        }
+    }
+}
--- Lesson 3/Practice3.cs
+++ Lesson 3/Practice3.cs
@@ -2,4 +2,30 @@
 
 public class Practice3
 {
+    public void Switch()
+    {
+        Console.Write("Write a number : ");
+        var userInput = Console.ReadLine();
+
+        var isUserInputCorrect = int.TryParse(userInput, out var userInputInt);
+
+        switch (userInputInt)
+        {
+            case 0: 
+                {
+                    Console.WriteLine("0");
+                    break;
+                }
+            case 1: { Console.WriteLine("1"); break; }
+            case 2: { Console.WriteLine("2"); break; }
+            case 3: { Console.WriteLine("3"); break; }
+            case 4: { Console.WriteLine("4"); break; }
+            case 5: { Console.WriteLine("5"); break; }
+            case 6: { Console.WriteLine("6"); break; }
+            case 7: { Console.WriteLine("7"); break; }
+            default:
+                Console.WriteLine("not match");
+                break;
+        }
+    }
 }
