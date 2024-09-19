ff11eacdd3f48b330ea23acea01c72409724f816
katerinapolianska <katerinapolansky@gmail.com>
2024-09-19 22:05:08 +0100

    Lesson 3

---
+++ Lesson 3/While_Tasks.cs
@@ -0,0 +1,25 @@
+namespace Lesson_3;
+
+public class While_Tasks
+{
+    public void While3()
+    {
+        Console.Write("Write n: ");
+        var n = int.Parse(Console.ReadLine());
+
+        Console.Write("Write k: ");
+        var k = int.Parse(Console.ReadLine());
+        
+
+        while (n>k)
+        {
+            n -= k;
+            
+        }
+        Console.WriteLine(n);
+        
+
+        
+        
+    }
+}
